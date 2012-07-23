05eef27ab4dfcbd38baf02c6a6d4c805284f4457
Michael Lumish <michael.lumish+stackoverflow@gmail.com>
2012-07-23 11:11:53 -0400

    Added address, optional client-side rendering to server side template

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -12,9 +12,10 @@ exports.index = function(req, res){
  */
 
 exports.menu = function(req, res){
-  //req.deliverator.render(res, req.params.rid, "menu", {});
+  var address = new req.deliverator.api.Address("1 Main Street", "College Station", "TX", "77840", "5555555555")
+  req.deliverator.render(res, req.params.rid, "menu", {}, address, true);
 
-  req.deliverator.renderSimple(res, req.params.rid, "menu", {});
+  //req.deliverator.renderSimple(res, req.params.rid, "menu", {}, address);
   
   // var deliverator = req.deliverator;
   // deliverator.getData(req.params.rid, function(err, data){
@@ -22,7 +23,7 @@ exports.menu = function(req, res){
   //     console.log(err);
   //   } else {
   //     var menu = deliverator.getMenu(data);
-  //     var head = deliverator.getHead(data);
+  //     var head = deliverator.getHead(req.params.rid, data, address);
   //     res.render("menu", {menu: menu, head: head});
   //   }
   // });
